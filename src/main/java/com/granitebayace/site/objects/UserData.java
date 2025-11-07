cafdde03528b554d6868bd2ff8e859196f46f767
ChadwickZackery <94324421+SpencerNold@users.noreply.github.com>
2025-11-07 13:18:31 -0800

    SCRUM-131
    Extended database to support Sessions associated with UserData as per the acceptance criteria of SCRUM-131.

--- src/main/java/com/granitebayace/site/objects/UserData.java
+++ src/main/java/com/granitebayace/site/objects/UserData.java
@@ -1,8 +1,8 @@
 package com.granitebayace.site.objects;
 
-public record UserData(String username, String passhash, Role role) {
+public record UserData(String username, String passhash, Session session, Role role) {
     @Override
     public String toString() {
-        return String.format("{name: %s, hash: %s, role: %s}", username, passhash, role.toString());
+        return String.format("{name: %s, hash: %s, session: %s, role: %s}", username, passhash, String.valueOf(session), String.valueOf(role));
     }
 }
