d6d6121bf16fe79285bc7bf2551ca31b9db65d50
alyssamjimenez <alyssa.jimenez@gmail.com>
2026-02-06 23:37:06 -0800

    Bug fix/Removed Duplicate Code
    Unnecessary code removed from RootController

--- src/main/java/com/granitebayace/site/services/RootController.java
+++ src/main/java/com/granitebayace/site/services/RootController.java
@@ -71,58 +71,6 @@ public class RootController extends Implementation {
         return Resource.Companion.get("pages/roles.html");
     }
 
-
-    //START IMAGE SECTION
-    //Put cacheControl of each image to no-cache so client can update the images frequently, i.e. revalidate on every call
-    @Route.File(path = "/images/page01.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img01() {
-        return Resource.Companion.get("images/page01.png");
-    }
-
-    @Route.File(path = "/images/page02.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img02() {
-        return Resource.Companion.get("images/page02.png");
-    }
-
-    @Route.File(path = "/images/page03.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img03() {
-        return Resource.Companion.get("images/page03.png");
-    }
-
-    @Route.File(path = "/images/page04.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img04() {
-        return Resource.Companion.get("images/page04.png");
-    }
-
-    @Route.File(path = "/images/page05.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img05() {
-        return Resource.Companion.get("images/page05.png");
-    }
-
-    @Route.File(path = "/images/page06.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img06() {
-        return Resource.Companion.get("images/page06.png");
-    }
-
-    @Route.File(path = "/images/page07.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img07() {
-        return Resource.Companion.get("images/page07.png");
-    }
-
-    @Route.File(path = "/images/page08.png", contentType = Route.ContentType.PNG,
-            cacheControl = "public, no-cache")
-    public InputStream img08() {
-        return Resource.Companion.get("images/page08.png");
-    }
-    //END IMAGE SECTION
-
     private DatabaseLayer getDatabase() {
         return getService(DatabaseLayer.class);
     }
